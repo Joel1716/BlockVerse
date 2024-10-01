002cc4ad2135628f04e8c11aaa379e28e028a073
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2024-10-01 13:03:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import dayjs from "https://unpkg.com/dayjs@1.8.9/esm/index.js";
 const timer = document.querySelector(".js-time");
 const dateLibrary = dayjs();
-const currentTime = dateLibrary.format("h:m");
+const currentTime = dateLibrary.format("H:mm");
 timer.innerHTML = currentTime;
